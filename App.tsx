b67077bc82702f23d016a03ad0ca26a715051267
ahmed mohamed <m_rx_22@yahoo.com>
2024-06-23 20:01:24 +0300

    add analytics & crashlytics

--- App.tsx
+++ App.tsx
@@ -6,20 +6,20 @@
  */
 
 import React, {useEffect} from 'react';
-import DefaultReactNativePaperTheme from './src/Utils/DefaultReactNativePaperTheme';
 import {StatusBar} from 'react-native';
-import {PersistGate} from 'redux-persist/integration/react';
-import SplashScreen from 'react-native-splash-screen';
-import {PaperProvider} from 'react-native-paper';
 import {Provider} from 'react-redux';
 import Toast from 'react-native-toast-message';
+import {PaperProvider} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 import messaging from '@react-native-firebase/messaging';
+import {PersistGate} from 'redux-persist/integration/react';
 
+import DefaultReactNativePaperTheme from './src/Utils/DefaultReactNativePaperTheme';
+import PushController from './src/Utils/PushController/PushController';
 import AppNavigation from './src/Routes/AppNavigation';
 import {store, persistor} from './src/Redux/Store';
-import './src/Localization/i18n';
 import toastConfig from './src/Utils/toastConfig';
-import PushController from './src/Utils/PushController/PushController';
+import './src/Localization/i18n';
 
 function App(): React.JSX.Element {
   const onBeforeLift = () => {
